5e041f93c44616134130ad9d0d59e8a72870ff3a
Nick Servidio <servidionicholas@gmail.com>
2018-04-15 22:47:27 -0400

    Initial Commit

---
+++ test_script/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  test_script
+//
+//  Created by Nick Servidio on 4/15/18.
+//  Copyright © 2018 Nick Servidio. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
